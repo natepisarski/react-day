2be84f206d6323f803c6a0d7eab9242f11517f40
Nathaniel Pisarski <nathanpisarski@gmail.com>
2019-03-07 20:27:39 -0500

    Add a redux store, reducers, and middleware

---
+++ src/RealApp.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+import {Provider} from "react-redux";
+import App from "./App";
+import {applyMiddleware, compose, createStore} from "redux";
+import reduceReducers from 'reduce-reducers';
+
+class RealApp extends Component {
+
+    reduxDevTools = () => window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+    loggingMiddleware = store => next => action => { console.debug(action); next(action) };
+
+    render() {
+        const defaultStoreReducer = (state = {}, action) => state;
+
+        const ourStoresStore = createStore(
+            reduceReducers(defaultStoreReducer),
+            {hello: 'Redux Message'},
+            compose(this.reduxDevTools(), applyMiddleware(this.loggingMiddleware))
+        );
+
+        return (
+            <Provider store={ourStoresStore}>
+                <div className="App">
+                    <Router>
+                        <Route exact path="/" component={App}/>
+                    </Router>
+                </div>
+            </Provider>
+        );
+    }
+}
+
+export default RealApp;
